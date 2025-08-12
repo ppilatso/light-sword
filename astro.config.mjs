aed8461839c9c70c09acbd993b81660f90f16590
Phenyo Pilatso <phenyo.pilatso@entelect.co.za>
2025-08-12 20:53:31 +0200

    Move to AWS

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,8 @@ import compress from "astro-compress";
 import icon from "astro-icon";
 import { defineConfig } from 'astro/config';
 
-import vercel from "@astrojs/vercel/serverless";
-
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), icon(), compress()],
-  output: "server",
-  adapter: vercel()
+  output: "static"
 });
